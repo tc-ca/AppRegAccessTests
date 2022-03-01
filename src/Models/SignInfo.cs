75fff5c26f92bdde323ad9f066c78071c49ab507
Asuquo Eniang <asuquo.eniang@034gc.onmicrosoft.com>
2022-03-01 15:31:33 -0500

    Added code for json file generation.

---
+++ src/Models/SignInfo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace AppRegAppTests.Models
+{
+    public class SignInfo
+    {
+        private string emailSubject = "Please sign this certificate";
+        private int maxSubjectLength;
+
+        public SignInfo()
+        {
+        }
+        public int NumberOfDocuments { get; set; }
+        public string SendersName { get; set; }
+        public string SenderEmail { get; set; }
+        public string SignersName { get; set; }
+        public string SignersEmail { get; set; }
+        public string[] HtmlContent { get; set; }
+        public int[] SignatureXPosition { get; set; }
+        public int[] SignatureYPosition { get; set; }
+        public string[] DocumentName { get; set; }
+        public string EmailSubject
+        {
+            get { return emailSubject; }
+            set
+            {                
+                maxSubjectLength = 100;
+                emailSubject = value.Length < maxSubjectLength ? value : value.Substring(0, maxSubjectLength - 4) + "...";
+            }
+        }
+        //public Uri ReturnUrl { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
--- src/Pages/ParseFile.cshtml.cs
+++ src/Pages/ParseFile.cshtml.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using AppRegAccessTests.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using AppRegAppTests.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace AppRegAccessTests.Pages
 {
@@ -15,7 +17,11 @@ namespace AppRegAccessTests.Pages
         public ParseFileModel(IWebHostEnvironment webHostEnv)
         {
             _webHostEnv = webHostEnv;
-            _fileName = Path.Combine(_webHostEnv.WebRootPath, "doc", "PreliminaryCertificate.html");
+            //_fileName = Path.Combine(_webHostEnv.WebRootPath, "doc", "PreliminaryCertificate.html");
+            _fileName = Path.Combine(_webHostEnv.WebRootPath, "doc", "PreliminaryCertificate_Activity_82.html");
+
+            //Byte[] bytes = System.IO.File.ReadAllBytes(_fileName);
+            //String fileB64 = Convert.ToBase64String(bytes);
         }
 
         public string FileName { get; private set; }
@@ -23,24 +29,49 @@ namespace AppRegAccessTests.Pages
 
         public void OnGet()
         {
-            FileName = _fileName;
-            //FileContent = GetFileContent(_fileName);
+            GetSignInfoJsonObject();
+            #region use fixture
 
-            var fixture = new Fixture();
-            PrelimCertModel model = fixture.Create<PrelimCertModel>();
-            
-            Random rnd = new Random();
-            int index = rnd.Next(0, 2);
+            //FileName = _fileName;
+            //var fixture = new Fixture();
+            //PrelimCertModel model = fixture.Create<PrelimCertModel>();            
+            //Random rnd = new Random();
+            //int index = rnd.Next(0, 2);
+            //model.TypeOfCargo = new string[] { "Concentrates", "Grain", "Timber" }[index];
+            //fixture.AddManyTo<DeficiencyDetail>(model.DeficiencyDetails, rnd.Next(1, 20));
+            //IDictionary<string, string> modelDict = GetPrelimModelDictionary(model);
+            //var outputFilename = _fileName.Replace(".html", $"_def_{model.DeficiencyId}.html");
+            //string certHtmlString = GetCertificateHtmlString(_fileName, modelDict, outputFilename);
+            //FileContent = certHtmlString;
 
-            model.TypeOfCargo = new string[] { "Concentrates", "Grain", "Timber" }[index];
-            fixture.AddManyTo<DeficiencyDetail>(model.DeficiencyDetails, rnd.Next(1, 20));
-            IDictionary<string, string> modelDict = GetPrelimModelDictionary(model);
+            #endregion
+        }
 
-            var outputFilename = _fileName.Replace(".html", $"_def_{model.DeficiencyId}.html");
-            string certHtmlString = GetCertificateHtmlString(_fileName, modelDict, outputFilename);
+        private void GetSignInfoJsonObject()
+        {
+            var signInfo = new SignInfo
+            {
+                //NumberOfDocuments = 1,
+                SendersName = "Asuquo Eniang",
+                SenderEmail = "asuquo.eniang@tc.gc.ca",
+                SignersName = "Asuquo Eniang",
+                SignersEmail = "asuquo.eniang@tc.gc.ca",
+                SignatureXPosition = new int[] { 100 },
+                SignatureYPosition = new int[] { 100 },
+                DocumentName = new string[] { "PreliminaryCertificate_def_205.pdf" },
+                EmailSubject = "Please sign preliminary certificate",
+                ReturnUrl = "https://localhost:7078/ParseFile"
+            };
+            string htmlContent = System.IO.File.ReadAllText(_fileName);
 
-            FileContent = certHtmlString;// String.Join("\n", 
-                //modelDict.Select(kvp => { return $"{kvp.Key}: {kvp.Value}"; }).ToArray());
+            signInfo.HtmlContent = new string[] { htmlContent };
+
+            // write to jason
+            var jsonFilename = _fileName.Replace(".html", ".json");
+            string json = JsonSerializer.Serialize(signInfo);
+            System.IO.File.WriteAllText(jsonFilename, json);
+
+            FileName = jsonFilename;
         }
 
         private string GetCertificateHtmlString(string inputFilename, IDictionary<string, string> model, string outputFilename)
@@ -63,7 +94,8 @@ namespace AppRegAccessTests.Pages
                 {
                     nodeSelector = "//tbody[@id='deficiencies']";
                     HtmlNode targetNode = doc.DocumentNode.SelectSingleNode(nodeSelector);
-                    if (targetNode != null) {
+                    if (targetNode != null)
+                    {
                         HtmlNode deficiencyRowNode = HtmlNode.CreateNode(kvp.Value);
                         targetNode.AppendChild(deficiencyRowNode);
                     }
@@ -72,13 +104,13 @@ namespace AppRegAccessTests.Pages
                 {
                     nodeSelector = $"//b[@name='{kvp.Key}']";
                     HtmlNode targetNode = doc.DocumentNode.SelectSingleNode(nodeSelector);
-                    if (targetNode != null) targetNode.InnerHtml = kvp.Value; 
+                    if (targetNode != null) targetNode.InnerHtml = kvp.Value;
                 }
             }
 
-            if(!string.IsNullOrEmpty(outputFilename)) doc.Save(outputFilename);
+            if (!string.IsNullOrEmpty(outputFilename)) doc.Save(outputFilename);
             content = doc.Text;
-            
+
             return content;
         }
 
@@ -95,7 +127,7 @@ namespace AppRegAccessTests.Pages
                     var deckArea = "";
                     var requirements = "";
                     //style="height: 425px"
-                    int rowHeight = 425/ model.DeficiencyDetails.Count;
+                    int rowHeight = 425 / model.DeficiencyDetails.Count;
 
                     for (int i = 0; i < model.DeficiencyDetails.Count; i++)
                     {
@@ -105,9 +137,9 @@ namespace AppRegAccessTests.Pages
                         key = $"{DeckAreaKey}_{i}";
                         rowHtml = $"<tr style=\"border:none;height:{rowHeight}px;\" >" +
                                     $"<td scope=\"col\"><b>{deckArea}</b></td>" +
-                                    $"<td scope=\"col\"><b>{requirements}</b></td>" +    
+                                    $"<td scope=\"col\"><b>{requirements}</b></td>" +
                                   "</tr>";
-                 
+
                         dict.Add(key, rowHtml);
                     }
                 }
